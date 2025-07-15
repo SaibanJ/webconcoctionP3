e2efa364ad52ce302c9b6ddb52e46c404e5640e3
SaibanJ <saibanjamesr@gmail.com>
2025-07-15 14:41:51 -0400

    resolved build issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Mona_Sans as FontSans } from "next/font/google"
-import "../styles/globals.css"
+import "./globals.css"
 import { cn } from "@/lib/utils"
 
 const fontSans = FontSans({
